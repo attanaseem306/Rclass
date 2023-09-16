42216a6ef04dafd131c82f0884459f00507d287a
attanaseem306 <attanaseem306@gmail.com>
2023-09-16 02:33:17 -0700

    'hello'

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import Cards from './Cards'
+import './sidebar.css'
+// import { db,addDoc,collection } from "./config/firebase";
 
 import {
   DesktopOutlined,
@@ -9,6 +11,9 @@ import {
   UserOutlined,
 } from '@ant-design/icons';
 import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { Radio } from 'react-bootstrap';
+
+
 const { Header, Content, Footer, Sider } = Layout;
 function getItem(label, key, icon, children) {
   return {
@@ -18,6 +23,7 @@ function getItem(label, key, icon, children) {
     label,
   };
 }
+
 const items = [
   
   getItem('User', 'sub1', <UserOutlined />, [
@@ -28,7 +34,35 @@ const items = [
   getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
   getItem('Files', '9', <FileOutlined />),
 ];
-const Sidebar = () => {
+const Sidebar = async() => {
+
+  // document.getElementById("btn").addEventListener('click', async () => {
+  
+  // let title=document.getElementById("title");
+  // let img=document.getElementById("img").files;
+  // let price=document.getElementById("price");
+  // let Category=document.getElementById("Category");
+  // let Discription=document.getElementById("Discription");
+
+  // try {
+  //   const docRef = await addDoc(collection(db, "item"), {
+  //     Title:title.value,
+  //     Price:price.value,
+  //     Category:Category.value,
+  //     Discription:Discription.value
+
+  //   });
+  //   console.log("Document written with ID: ", docRef.id);
+  // } catch (e) {
+  //   console.error("Error adding document: ", e);
+  // }
+  // })
+
+  const num=' > 100'
+  const num2=' > 200'
+  const num3=' > 300'
+  const num4= ' > 400'
+
   const [collapsed, setCollapsed] = useState(false);
   const {
     token: { colorBgContainer },
@@ -38,11 +72,27 @@ const Sidebar = () => {
       style={{
         minHeight: '100vh',
       }}
+      
     >
-      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+      <Sider  className='side' collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+        
         <div className="demo-logo-vertical" />
-        <div>
-        <h5 style={{color:'green', textAlign:'center', marginTop:30}}>Filter</h5>
+        <div className='main'>
+        <h5  className='firstH'>Filter</h5>
+        <input className='input1' type="text" placeholder='Input seacrh text' id="Sort"/>
+        <h6 className='secondH' >Price</h6>
+        <div className='same'><input type="radio"/><h6 className='sameH'>{num}</h6></div>
+        <div className='same'><input type="radio"/><h6 className='sameH'>{num2}</h6></div>
+        <div className='same'><input type="radio"/><h6 className='sameH'>{num3}</h6></div>
+        <div className='same'><input type="radio"/><h6 className='sameH'>{num4}</h6></div>
+        <h6 className='thirdH'>Sort By</h6>
+        <div className='same'><input type="radio"/><h6 className='high'>H to L</h6>
+        <div className='second'>< input type="radio"/><h6 className='high'>L to H</h6></div>
+        </div>
+        <h6 className='fourthH'>Sort By Categories</h6>
+        <div className='same'><input type="radio"/><h6 className='sameinput'>Mobile</h6></div>
+        <div className='same'><input type="radio"/><h6 className='sameinput'>Laptop</h6></div>
+        <div className='same'><input type="radio"/><h6 className='sameinput'>Cards</h6></div>
         </div>
       </Sider>
       <Layout>
@@ -71,7 +121,19 @@ const Sidebar = () => {
               background: colorBgContainer,
             }}
             >
-          <div style={{display:'flex', justifyContent:'space-around' , flexWrap : 'wrap'}}>
+            <div className='secondMain'>
+              <div className='secondlite'>
+                <input className='sameinput2' type='text' placeholder='Service Title' id='title' />
+                <input className='sameinput2' type='file' id='img' />
+                <input className='sameinput2' type='number' placeholder='Price' id='price' />
+                </div>
+                <div className='newDiv'>
+                <input  className='match' type='text' placeholder='Category' id='Category' />
+                <input className='match' type='text' placeholder='Discription' id='Discription' />
+                <button className='btn' type='number' placeholder='Price' id='btn'>Add / Edit Service</button>
+                </div>
+            </div>
+          <div className='Cards'>
         <Cards/>
         <Cards/>
         <Cards/>
---
+++ src/config/firebase.jsx
@@ -0,0 +1,22 @@
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import { getFirestore} from "firebase/firestore";
+// import { addDoc,collection} from "firebase/firestore";
+
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCrAJfYgMc2B5skStq-1L4RoXvPbMH8jSA",
+//   authDomain: "reactfirst-69715.firebaseapp.com",
+//   projectId: "reactfirst-69715",
+//   storageBucket: "reactfirst-69715.appspot.com",
+//   messagingSenderId: "359520861525",
+//   appId: "1:359520861525:web:14bcdd3d51aed0208083dc",
+//   measurementId: "G-8F76M5F4MT"
+// };
+
+
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// const db = getFirestore(app);
+
+// export{db,addDoc,collection}
